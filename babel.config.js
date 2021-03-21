bd9e0070fe9e811a6f2772bfa44ed94dc8f04409
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2021-03-21 14:32:01 +0900

    Hide API key.

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
 module.exports = function(api) {
   api.cache(true);
   return {
+    "plugins": [
+      ["module:react-native-dotenv", {
+        "blacklist": [
+          "GITHUB_TOKEN"
+        ]
+      }]
+    ],
     presets: ['babel-preset-expo'],
   };
 };
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,25 @@
 import * as firebase from 'firebase';
 import 'firebase/firestore';
+import {
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+} from '@env';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyByOo2xUdV1RaXcwwMc0hmKZh1w1IOn_uI",
-  authDomain: "sns-app-507c8.firebaseapp.com",
-  databaseURL: "https://sns-app-507c8.firebaseio.com",
-  projectId: "sns-app-507c8",
-  storageBucket: "sns-app-507c8.appspot.com",
-  messagingSenderId: "928456747036",
-  appId: "1:928456747036:web:9d116164e4b524ff6657d9",
-  measurementId: "G-Z9K5CSN75E"
+  apiKey: apiKey,
+  authDomain: authDomain,
+  databaseURL: databaseURL,
+  projectId:　projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId,
 };
 
 if (!firebase.apps.length) {
